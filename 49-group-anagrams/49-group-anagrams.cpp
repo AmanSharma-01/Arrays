1d7a6bb9953bfd170d3056057ce57d66c42b6354
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-09 12:45:44 +0530

    Time: 230 ms (6.60%), Space: 310.1 MB (5.01%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
+    
+    string countSort(string s) {
+        vector<int> dp(26, 0);
+        for(auto ch: s) dp[ch-'a']++;
+        string ans="";
+        for(int i=0;i<26;i++) {
+            ans += string(i+'a', dp[i]);
+        }
+        return ans;
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
         vector<vector<string>> ans;
         
         for(int i=0;i<strs.size();i++) {
-            string str = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            mp[strs[i]].push_back(str);
+            // string str = strs[i];
+            // sort(strs[i].begin(), strs[i].end());
+            mp[countSort(strs[i])].push_back(strs[i]);
         }
         
         for(auto i: mp) {
