c26b53aec6040ee9323862036f193b07b0a24993
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-13 22:17:05 +0530

    Time: 14 ms (34.72%), Space: 6.7 MB (36.33%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    
+    bool isValid(vector<string>& board, int r, int c) {
+        int n = board.size();
+        for(int i=0;i<r;i++) if(board[i][c]=='Q') return false;
+        int i=r-1, j=c-1;
+        while(i>=0&&j>=0) if(board[i--][j--]=='Q') return false;
+        i=r-1,j=c+1;
+        while(i>=0&&j<n) if(board[i--][j++]=='Q') return false;
+        
+        return true;
+    }
+    
+    void dfs(vector<string>& board, int row, int n) {
+        if(row >= n) {
+            ans.push_back(board);
+            return;
+        }
+        for(int i=0;i<n;i++) {
+            board[row][i]='Q';
+            if(isValid(board, row, i)) {
+                dfs(board, row+1, n);
+            }
+            board[row][i]='.';
+        }
+    }
+    
+    int totalNQueens(int n) {
+        string temp = "";
+        for(int i=0;i<n;i++) temp+='.';
+        vector<string> board(n ,temp);
+        dfs(board, 0, n);
+        return ans.size();
+    }
+};
