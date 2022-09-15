09a463da7656811564f9284a324e7cbb5023ec0c
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-15 11:13:45 +0530

    Time: 11 ms (41.24%), Space: 7.9 MB (73.05%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,19 +3,20 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1, 0);
         ans[0]=0;
-        for(int i=0;i<=n;i++) {
+        for(int i=1;i<=n;i++) {
             ans[i] = ans[i/2]+i%2;
         }
-        
-        // for(int i=0;i<=n;i++) {
-        //     int count=0;
-        //     int x = i;
-        //     while(x>0) {
-        //         count+=x%2;
-        //         x = x/2;
-        //     }
-        //     ans[i]=count;
-        // }
         return ans;
+//         for(int i=0;i<=n;i++) {
+//             int num = i;
+//             int count = 0;
+            
+//             while(num > 0) {
+//                 count += num%2;
+//                 num = num/2;
+//             }
+//             ans[i] = count;
+//         }
+//         return ans;
     }
 };
