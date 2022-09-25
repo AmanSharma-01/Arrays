66094ad22b3a4a28ac73cbe2419eee6447defd73
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-25 15:04:14 +0530

    Time: 47 ms (32.75%), Space: 14 MB (54.25%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,33 +1,60 @@
 class Solution {
 public:
     
-    bool canComplete(int course, unordered_map<int, vector<int>>& mp, vector<bool>& visited, vector<bool>& completed) {
-        if(completed[course]) return true;
-        if(visited[course]) return false;
+//     bool canComplete(int course, unordered_map<int, vector<int>>& mp, vector<bool>& visited, vector<bool>& completed) {
+//         if(completed[course]) return true;
+//         if(visited[course]) return false;
         
-        visited[course] = true;
-        for(auto nc: mp[course]) {
-            if(canComplete(nc, mp, visited, completed)) continue;
-            else return false;
-        }
-        completed[course] = true;
-        return true;
-    }
+//         visited[course] = true;
+//         for(auto nc: mp[course]) {
+//             if(canComplete(nc, mp, visited, completed)) continue;
+//             else return false;
+//         }
+//         completed[course] = true;
+//         return true;
+//     }
     
     bool canFinish(int num, vector<vector<int>>& prerequisites) {
+        
         int n = prerequisites.size();
         unordered_map<int, vector<int>> mp;
-        vector<bool> visited(num+1, false);
-        vector<bool> completed(num+1, false);
+        vector<int> count(num+1, 0);
+        queue<int> q;
+        int res=0;
         
         for(int i=0;i<n;i++) {
-            mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            mp[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            count[prerequisites[i][0]]++;
         }
         
         for(int i=0;i<num;i++) {
-            if(canComplete(i, mp, visited, completed)) continue;
-            else return false;
+            if(count[i] == 0) q.push(i);
         }
-        return true;
+        
+        while(!q.empty()) {
+            res++;
+            int top = q.front();q.pop();
+            for(auto nc: mp[top]) {
+                count[nc]--;
+                if(count[nc]==0) q.push(nc);
+            }
+        }
+        return res==num;
+        
+        
+//         int n = prerequisites.size();
+//         unordered_map<int, vector<int>> mp;
+//         vector<bool> visited(num+1, false);
+//         vector<bool> completed(num+1, false);
+        
+//         for(int i=0;i<n;i++) {
+//             mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+//         }
+        
+//         for(int i=0;i<num;i++) {
+//             if(canComplete(i, mp, visited, completed)) continue;
+//             else return false;
+//         }
+//         return true;
     }
 };
