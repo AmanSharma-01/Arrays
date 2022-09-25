6ef1c0d2a378ffcedd4acb9dde3a71a3e2ece338
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-25 22:53:19 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (29.40%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -4,30 +4,35 @@ public:
         vector<int> ans;
         int m = mat.size();
         int n = mat[0].size();
-        int top=0, bottom=m-1, left=0, right=n-1;
+        int top=0,bottom=m-1,right=n-1,left=0;
         int dir=1;
-        while(ans.size() != n*m) {
+        
+        while(ans.size() != m*n) {
             if(dir == 1) {
                 for(int i=left;i<=right;i++) ans.push_back(mat[top][i]);
-                dir=2;
                 top++;
+                dir=2;
             }
+            
             else if(dir == 2) {
                 for(int i=top;i<=bottom;i++) ans.push_back(mat[i][right]);
-                dir=3;
                 right--;
+                dir=3;
             }
+            
             else if(dir == 3) {
                 for(int i=right;i>=left;i--) ans.push_back(mat[bottom][i]);
-                dir=4;
                 bottom--;
+                dir=4;
             }
+            
             else if(dir == 4) {
                 for(int i=bottom;i>=top;i--) ans.push_back(mat[i][left]);
-                dir=1;
                 left++;
+                dir=1;
             }
         }
+        
         return ans;
     }
 };
