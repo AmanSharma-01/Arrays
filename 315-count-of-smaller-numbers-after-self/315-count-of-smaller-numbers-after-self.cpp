ba2517f5a95dd3fff13de5ebb8ab05f6fc0df8e6
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-26 15:24:36 +0530

    Time: 1293 ms (33.55%), Space: 248.4 MB (30.85%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     
-    void helper(vector<pair<int,int>>& arr, vector<int>& ans, int st, int ed) {
-        if(ed-st <= 0) return;
+    void countLess(vector<pair<int, int>>& arr, vector<int>& ans, int st, int ed) {
+        if(st>=ed) return;
         int mid = st + (ed-st)/2;
-        helper(arr, ans, st, mid);
-        helper(arr, ans, mid+1, ed);
+        countLess(arr, ans, st, mid);
+        countLess(arr, ans, mid+1, ed);
         
         int j = mid+1;
         for(int i=st;i<=mid;i++) {
-            while(j<=ed && arr[j].first < arr[i].first) j++;
+            while(j<=ed && arr[i].first>arr[j].first) j++;
             ans[arr[i].second] += j-(mid+1);
         }
         inplace_merge(arr.begin()+st, arr.begin()+mid+1, arr.begin()+ed+1);
@@ -17,12 +17,12 @@ public:
     
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
+        vector<pair<int, int>> arr;
         vector<int> ans(n, 0);
-        vector<pair<int,int>> arr;
         for(int i=0;i<n;i++) {
             arr.push_back({nums[i], i});
         }
-        helper(arr, ans, 0, n-1);
+        countLess(arr, ans, 0, n-1);
         return ans;
     }
 };
