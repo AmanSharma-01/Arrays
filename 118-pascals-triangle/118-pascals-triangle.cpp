ca976d62b8e2fa5a05841d32131e0da57e31f10d
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-26 15:08:42 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.16%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -8,12 +8,13 @@ public:
             vector<int> temp;
             temp.push_back(1);
             for(int j=1;j<i;j++) {
-                int a = ans[i-1][j-1] + ans[i-1][j];
+                int a = ans[i-1][j]+ans[i-1][j-1];
                 temp.push_back(a);
             }
             temp.push_back(1);
             ans.push_back(temp);
         }
+        
         return ans;
     }
 };
