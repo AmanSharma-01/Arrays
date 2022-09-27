352d5ac069abe9448d6102bc7688ff7c9cfae854
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-27 14:18:58 +0530

    Time: 67 ms (45.77%), Space: 13.8 MB (49.85%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<pair<int,int>>> mp;
-        queue<pair<int, int>> q;
-        vector<int> d(n ,1e9);
-        
-        for(auto flight: flights)
+        unordered_map<int,vector<pair<int,int>>> mp;
+        for(auto flight: flights) {
             mp[flight[0]].push_back({flight[1], flight[2]});
-        
+        }
+        vector<int> price(n, 1e9);
+        price[src]=0;
+        queue<pair<int, int>> q;
         q.push({src, 0});
-        d[src]=0;
-        int count=0; // number of stops till now.
+        
+        int count =0;
         
         while(!q.empty()) {
             for(int i=q.size();i;i--) {
                 auto [node, val] = q.front();q.pop();
                 
                 for(auto nxt: mp[node]) {
-                    if(val+nxt.second < d[nxt.first]) {
-                        d[nxt.first] = val+nxt.second;
-                        q.push({nxt.first, d[nxt.first]});
+                    if(nxt.second+val < price[nxt.first]) {
+                        price[nxt.first] = nxt.second+val;
+                        q.push({nxt.first, price[nxt.first]});
                     }
                 }
+                
             }
             count++;
-            if(count>k) return d[dst]==1e9 ? -1 : d[dst];
+            if(count>k) return price[dst] == 1e9 ? -1 : price[dst];
         }
-        return d[dst]==1e9 ? -1 : d[dst];
+        
+        return price[dst] == 1e9 ? -1 : price[dst];
     }
 };
