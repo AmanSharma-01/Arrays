46debd5ffe9399bf967afbdc278b8e629eb1c7e6
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-09-29 13:00:03 +0530

    Time: 695 ms (18.55%), Space: 12.8 MB (59.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int8_t dp[201][10001] = {[0 ... 200] = {[0 ... 10000] = -1}};
-    
-    bool isPossible(vector<int>& nums, int tar, int i=0) {
-        if(tar == 0) return true;
-        if(i>=nums.size() || tar < 0) return false;
-        if(dp[i][tar] != -1) return dp[i][tar];
-        return dp[i][tar] = isPossible(nums, tar-nums[i], i+1) || isPossible(nums, tar, i+1);
-    }
-    
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int total = accumulate(nums.begin(), nums.end(), 0);
         if(total%2) return false;
         int tar = total/2;
-        return isPossible(nums, tar);
+        
+        vector<vector<bool>> dp(n+1, vector<bool> (tar+1, false));
+        for(int i=0;i<tar+1;i++) dp[0][i]=false;
+        for(int i=0;i<n+1;i++) dp[i][0]=true;
+        
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<tar+1;j++) {
+                if(nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][tar];
     }
 };
