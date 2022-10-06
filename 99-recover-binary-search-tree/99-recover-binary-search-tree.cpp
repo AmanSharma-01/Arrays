be16bb875f0da561dc18d2a3cdeeb516a240066b
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-06 23:03:40 +0530

    Time: 75 ms (21.22%), Space: 57.9 MB (45.13%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -10,27 +10,53 @@
  * };
  */
 class Solution {
+private: 
+    TreeNode* first, *prev, *middle, *last;
 public:
-    vector<int> ans;
-    int idx;
+//     vector<int> ans;
+//     int idx;
+//     void inorder(TreeNode* root) {
+//         if(!root) return;
+//         inorder(root->left);
+//         ans.push_back(root->val);
+//         inorder(root->right);
+//     }
+    
+//     void inorder2(TreeNode* root) {
+//         if(!root) return;
+//         inorder2(root->left);
+//         root->val = ans[idx++];
+//         inorder2(root->right);
+//     }
+    
+    
     void inorder(TreeNode* root) {
         if(!root) return;
         inorder(root->left);
-        ans.push_back(root->val);
+        
+        if(prev != NULL && root->val < prev->val) {
+            if(first == NULL) {
+                first = prev;
+                middle = root;
+            } else {
+                last = root;
+            }
+        }
+        
+        prev = root;
         inorder(root->right);
     }
     
-    void inorder2(TreeNode* root) {
-        if(!root) return;
-        inorder2(root->left);
-        root->val = ans[idx++];
-        inorder2(root->right);
-    }
-    
     void recoverTree(TreeNode* root) {
+        first=middle=last=NULL;
+        prev = new TreeNode(INT_MIN);
         inorder(root);
-        idx=0;
-        sort(ans.begin(), ans.end());
-        inorder2(root);
+        if(first && last) swap(first->val, last->val);
+        else if(first && middle) swap(first->val, middle->val);
+        
+        // inorder(root);
+        // idx=0;
+        // sort(ans.begin(), ans.end());
+        // inorder2(root);
     }
 };
