be9ffc6228f4fce69b486e24adbbcadcbde0ebfa
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-09 22:29:20 +0530

    Time: 3 ms (99.45%), Space: 9.1 MB (48.36%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,6 +1,7 @@
 class UF {
-    public:
+public:
     vector<int> par;
+    
     UF(int n) {
         for(int i=0;i<n;i++) {
             par.push_back(i);
@@ -15,46 +16,29 @@ class UF {
     bool Union(int n1, int n2) {
         int p1 = find(n1);
         int p2 = find(n2);
-        if(p1 == p2) return true;
-        par[p1] = p2;
+        if(p1==p2) return true;
+        par[p1]=p2;
         return false;
     }
     
 };
 
-
-
-
 class Solution {
 public:
-    
-    // bool canReach(int e1, int e2, unordered_map<int, vector<int>>& mp, vector<bool>& vis) {
-    //     if(vis[e1]) return true;
-    //     vis[e1] = true;
-    //     for(auto nxt: mp[e1]) {
-    //         if(nxt != e2 && canReach(nxt, e1, mp, vis)) return true;
-    //     }
-    //     return false;
-    // }
-    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        UF* uf = new UF(n+1);
+        UF *uf = new UF(n+1);
         
-        for(auto i: edges) {
-            if(uf->Union(i[0], i[1])) return i;
+        for(auto edge: edges) {
+            if(uf->Union(edge[0], edge[1])) return edge;
         }
-        return {};
         
-//         unordered_map<int, vector<int>> mp;
-//         vector<bool> vis(n+1, false);
-//         for(int i=0;i<edges.size();i++) {
-           
-//             for(int j=0;j<n+1;j++)vis[j]=false;
-//             mp[edges[i][0]].push_back(edges[i][1]);
-//             mp[edges[i][1]].push_back(edges[i][0]);
-//             if(canReach(edges[i][0], -1, mp, vis)) return edges[i];
-//         }
-//         return {};
+        return {};
     }
 };
+
+
+
+
+
+
