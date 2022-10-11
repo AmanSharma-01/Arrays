001570d6605fb1c83cef75d17f222ae83e4022f0
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-11 15:48:07 +0530

    Time: 16 ms (48.96%), Space: 6.1 MB (49.53%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    string romanstring ( int rem, char a, char b, char c ) 
+    {
+        string abc="";
+        if(rem==0)return "";
+        else if(rem >= 1 && rem <=3) {for(int i=0;i<rem;i++) {abc+=a;} }
+        else if(rem == 4) {abc=abc+a+b;}
+        else if(rem == 5) {abc+=b;}
+        else if(rem == 9) {abc=abc+a+c;}
+        else
+        {
+            rem-=5;abc+=b;
+            for(int i=0;i<rem;i++) {abc+=a;} 
+        }
+        return abc;
+    }
+    
+    string intToRoman(int A) {
+        string ans = "";
+        int r = 0;
+        int d = 0;
+        r=A/1000;
+        A=A%1000;
+        //1 //5//10
+        ans+=romanstring(r,'M','Z','Z');
+        r=A/100;
+        A=A%100;
+        ans+=romanstring(r,'C','D','M');
+        r=A/10;
+        A=A%10;
+        ans+=romanstring(r,'X','L','C');
+        r=A/1;
+        A=A%1;
+        ans+=romanstring(r,'I','V','X');
+        return ans;
+    }
+};
