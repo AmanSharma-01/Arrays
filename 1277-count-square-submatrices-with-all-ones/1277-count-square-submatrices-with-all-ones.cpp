22462122f7fe1fcbbdaecfa54cdc6da1a20c624e
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-13 21:49:07 +0530

    Time: 174 ms (21.14%), Space: 23.6 MB (75.68%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -5,14 +5,12 @@ public:
         int n = mat[0].size();
         int ans=0;
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        
-        for(int i=1;i<=m;i++) {
-            for(int j=1;j<=n;j++) {
-                if(mat[i-1][j-1] == 1) {
-                    dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})+1;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(i!=0 && j!=0 && mat[i][j] == 1) {
+                    mat[i][j] = min({mat[i-1][j], mat[i][j-1], mat[i-1][j-1]})+1;
                 }
-                ans+=dp[i][j];
+                ans+=mat[i][j];
             }
         }
         
