6202a24eb2952af232e26c544b1a5f95001c8054
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-14 21:59:33 +0530

    Time: 19 ms (46.01%), Space: 10.3 MB (80.41%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>& rooms, vector<int>& vis, int room) {
-        if(vis[room]==1) return;
-        vis[room] = 1;
-        for(int i=0;i<rooms[room].size();i++) {
-            dfs(rooms, vis, rooms[room][i]);
-        }
-    }
+    // void dfs(vector<vector<int>>& rooms, vector<int>& vis, int room) {
+    //     if(vis[room]==1) return;
+    //     vis[room] = 1;
+    //     for(int i=0;i<rooms[room].size();i++) {
+    //         dfs(rooms, vis, rooms[room][i]);
+    //     }
+    // }
     
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<int> vis(n, -1);
-        dfs(rooms, vis, 0);
+        queue<int> q;
+        q.push(0);
+        vis[0]=1;
+        
+        while(!q.empty()) {
+            int top = q.front();q.pop();
+            vis[top] = 1;
+            for(int i=0;i<rooms[top].size();i++) {
+                if(vis[rooms[top][i]] == -1) {
+                    q.push(rooms[top][i]);
+                }
+            }
+        }
+//         dfs(rooms, vis, 0);
         
         for(int i=0;i<n;i++) {
             if(vis[i]==-1) {
