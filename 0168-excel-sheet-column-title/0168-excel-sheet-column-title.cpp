b0221a99d7e4ea2c629f80642843cf7a1d8a2698
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-21 00:13:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.66%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans = "";
+        n--;
+        while(n>=0) {
+            ans += ('A' + n%26);
+            n = n/26;
+            n--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
