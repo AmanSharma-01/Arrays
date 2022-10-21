09d5dc26dffa41a36d4a439b315e842ec71cd109
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-21 06:31:10 +0530

    Time: 4 ms (70.96%), Space: 6.5 MB (87.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> tfreq(26, 0);
-        vector<int> sfreq(26, 0);
+        char c = 0;
         
-        for(auto ch: s) sfreq[ch-'a']++;
-        for(auto ch: t) tfreq[ch-'a']++;
+        for(auto ch: s) c^=ch;
+        for(auto ch: t) c^=ch;        
         
-        for(int i=0;i<26;i++) {
-            if(tfreq[i]-sfreq[i] == 1) return 'a'+i;
-        }
+        return c;
         
-        return 'a';
+//         vector<int> tfreq(26, 0);
+//         vector<int> sfreq(26, 0);
+        
+//         for(auto ch: s) sfreq[ch-'a']++;
+//         for(auto ch: t) tfreq[ch-'a']++;
+        
+//         for(int i=0;i<26;i++) {
+//             if(tfreq[i]-sfreq[i] == 1) return 'a'+i;
+//         }
+        
+//         return 'a';
     }
 };
