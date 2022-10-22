9e34f03baa7bbfea79dcd57ff36ea9dbdbf330d6
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-10-22 22:14:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.26%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     
-    int getNum(int num) {
-        int sum=0;
-        while(num>0) {
-            int d = num%10;
-            sum+=d;
-            num=num/10;
-        }
-        return sum;
-    }
+//     int getNum(int num) {
+//         int sum=0;
+//         while(num>0) {
+//             int d = num%10;
+//             sum+=d;
+//             num=num/10;
+//         }
+//         return sum;
+//     }
     
     int addDigits(int num) {
-        if(num < 10) return num;
-        return addDigits(getNum(num));
+        if(num == 0) return 0;
+        else if(num%9 == 0) return 9;
+        else return num%9;
+        // if(num < 10) return num;
+        // return addDigits(getNum(num));
     }
 };
