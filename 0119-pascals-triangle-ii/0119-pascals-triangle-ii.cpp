df297102371c0a0561594dbfa689f93f698c73c7
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-11-07 00:46:18 +0530

    Time: 3 ms (50.70%), Space: 6.6 MB (18.12%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> getRow(int n) {
-        vector<vector<int>> dp;
-        dp.push_back({1});
+        vector<int> lastRow;
+        lastRow.push_back({1});
         
         for(int i=1;i<=n;i++) {
             vector<int> temp;
             temp.push_back(1);
             for(int j=1;j<i;j++) {
-                int a = dp[i-1][j]+dp[i-1][j-1];
+                int a = lastRow[j]+lastRow[j-1];
                 temp.push_back(a);
             }
             temp.push_back(1);
-            dp.push_back(temp);
+            lastRow = temp;
         }
-        return dp[n];
+        return lastRow;
     }
 };
