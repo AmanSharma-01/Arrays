373d046221737be40f9fe29bf62643613cf8da7d
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-11-14 13:58:25 +0530

    Time: 314 ms (100.00%), Space: 126.8 MB (50.00%) - LeetHub

--- 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -11,44 +11,41 @@
  */
 class Solution {
 private:
-    int findMinSwaps(vector<int>& nums) {
-        vector<pair<int,int>> v;
-	    int n=nums.size();
-	    for(int i=0;i<n;i++)
-	    {
-	        v.push_back({nums[i],i});
-	    }
-	    sort(v.begin(),v.end());
-	    int swaps=0;
-	    for(int i=0;i<n;i++)
-	    {
-	        if(v[i].second==i) continue;
-	        else {
-	            swaps++;
-	            swap(v[i],v[v[i].second]);
-	            i--;
-	        }
-	    }
-	    return swaps;
+    int findMinSwapsForLevel(vector<int>& nums) {
+        vector<pair<int, int>> arr;
+        int n = nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        for(int i=0;i<n;i++) {
+            if(arr[i].second == i) continue;
+            else {
+                ans++;
+                swap(arr[i], arr[arr[i].second]);
+                i--;
+            }
+        }
+        return ans;
     }
 public:
     int minimumOperations(TreeNode* root) {
-        int ans=0;
         queue<TreeNode*> q;
         q.push(root);
+        vector<int> temp;
+        int ans=0;
         
         while(!q.empty()) {
-            vector<int> temp;
             for(int i=q.size();i;i--) {
                 TreeNode* top = q.front();q.pop();
                 temp.push_back(top->val);
                 if(top->left) q.push(top->left);
                 if(top->right) q.push(top->right);
             }
-            // find min swaps for each level and sum all up.
-            ans += findMinSwaps(temp);
+            ans += findMinSwapsForLevel(temp);
+            temp.clear();
         }
-        
         return ans;
     }
 };
