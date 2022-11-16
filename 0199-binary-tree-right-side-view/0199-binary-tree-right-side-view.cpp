17eb263d87ec577421eb0b4edc01cf360db91189
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-11-16 18:47:23 +0530

    Time: 3 ms (87.37%), Space: 12.2 MB (10.27%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -10,17 +10,40 @@
  * };
  */
 class Solution {
-private:
-    void findRight(TreeNode* root, int h, vector<int>& ans) {
-        if(!root) return;
-        if(ans.size() <= h) ans.push_back(root->val);
-        findRight(root->right, h+1, ans);
-        findRight(root->left, h+1, ans);
-    } 
+// private:
+//     void findRight(TreeNode* root, int h, vector<int>& ans) {
+//         if(!root) return;
+//         if(ans.size() <= h) ans.push_back(root->val);
+//         findRight(root->right, h+1, ans);
+//         findRight(root->left, h+1, ans);
+//     } 
 public:
     vector<int> rightSideView(TreeNode* root) {
+        if(!root) return {};
         vector<int> ans;
-        findRight(root, 0, ans);
+        queue<TreeNode*> q;
+        TreeNode* temp;
+        q.push(root);
+        
+        while(!q.empty()) {
+            for(int i=q.size();i;i--) {
+                TreeNode *top = q.front();q.pop();
+                temp = top;
+                if(top->left) q.push(top->left);
+                if(top->right) q.push(top->right);
+            }
+            ans.push_back(temp->val);
+        }
         return ans;
+        // findRight(root, 0, ans);
+        // return ans;
     }
 };
+
+
+
+
+
+
+
+
