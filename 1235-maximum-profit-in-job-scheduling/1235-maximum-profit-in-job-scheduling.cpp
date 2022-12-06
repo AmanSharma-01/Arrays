92a5fca58eba2af0d9f64f83813b1df3b9febab6
AmanSharma-01 <65444947+AmanSharma-01@users.noreply.github.com>
2022-12-06 15:28:16 +0530

    Time: 344 ms (42.71%), Space: 55 MB (59.76%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    vector<int>dp;
+    int find(vector<vector<int>>& events,int idx,int end){
+        if(idx==events.size())
+            return 0;
+        if( events[idx][0] < end )
+            return find( events,idx+1,end);
+        if( dp[idx] != -1)
+            return dp[idx];
+        int res= max (events[idx][2]+find(events,idx+1,events[idx][1]),find(events,idx+1,end));
+        return dp[idx] = res;
+    }
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
+    {
+        vector<vector<int>>events(startTime.size(),vector<int>(3,0));
+        for(int i=0;i<startTime.size();i++)
+        {
+            events[i]={startTime[i],endTime[i],profit[i]};
+        }
+        sort(events.begin() , events.end());
+        int n = events.size();
+        dp.resize(n,-1);
+        return find(events , 0 , 0);
+    }
+};
